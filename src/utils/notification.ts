1f1c5fc94ab9ab808a952746de220e06d7ed07b3
agtt <csd@hotmail.com.tr>
2025-04-15 12:28:37 +0100

    Refactor sendCompletionNotification to accept provider and completion check as parameters

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,12 +1,25 @@
 import { env } from "@/env";
 import { MailProvider } from "@/types";
 
-export const sendCompletionNotification = async (
-  send: MailProvider["send"],
-) => {
-  await send(
-    env.MAIL_RECEIPENT,
-    "All tasks completed",
-    "Congratulations! You have completed all your tasks.",
-  );
+interface INotification {
+  provider: MailProvider;
+  areAllTodosComplete: () => Promise<boolean>;
+}
+
+export const sendCompletionNotification = async ({
+  provider,
+  areAllTodosComplete,
+}: INotification): Promise<void> => {
+  try {
+    const allComplete = await areAllTodosComplete();
+    if (!allComplete) return;
+
+    await provider.send(
+      env.MAIL_RECEIPENT,
+      "All tasks completed",
+      "Congratulations! You have completed all your tasks.",
+    );
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
